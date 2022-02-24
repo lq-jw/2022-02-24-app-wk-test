d938483a1ac94a9f9aa52df16b0a7c09eb7e6570
laiqi <a21954262@gmail.com>
2022-02-24 09:39:23 +0800

    change

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text1}>I am going banana!!!</Text>
+      <Text style={styles.text3}>110919020 數位二甲</Text>
+      <Text style={styles.text3}>賴芊綺</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +19,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text1: {
+    fontSize: 20,
+    color: '#D8B1A5',
+  },
+  text2: {
+    fontSize: 30,
+    color: '#fff',
+  },
+  text3: {
+    fontSize: 30,
+    color: '#D8B1A5'
+  }
 });
